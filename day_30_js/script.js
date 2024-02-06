576504609d06294ae180800feddc91200af8fdb8
anuragstomar <anuragstomar121@gmail.comssh-keygen -t ed25519 -C anuragstomar121@gmail.com>
2024-02-06 12:58:13 +0530

    javascript basics

---
+++ day_30_js/script.js
@@ -0,0 +1,67 @@
+// console.log("hello thor")//console and how to add script in html
+// // {
+// //     2+5//giving comments in js by ctrl slash.
+// // }
+// //integer and float datatypes--->
+// //printing a number in js.
+// let number=10;
+// console.log(number)
+// //addition of two numbers in js.
+// let number1=9989;
+// let number2=11;
+// console.log(number1+number2)
+// //integer ,float
+// let number3=30.2;
+// console.log(number3)
+// //printing of a character
+// let word='anurag'
+// console.log(word)
+// //printing of a string
+// let Name="superman"//always use double code in string.
+// console.log(Name)
+
+// //bulean
+
+// let is18=false
+// console.log(is18)
+
+// //variables
+// let $dollar = 3000;
+// console.log("i have ", $dollar,"$")
+
+// let name1="superman"
+// let name2="spiderman"
+// console.log('"'+name1+'"'+"belongs from dc and"+ '"' +name2 +'"'+ "belongs from marvel")
+// //back-tick
+// let name3="superman";
+// let name4="spiderman";
+// console.log(`"${name3}" is belongs from 'DC' and"${name2}" is belong from 'marvel'`)
+
+// //changing the name in the string
+// let name5="superman";
+// let name6="spiderman";
+//  name5="antman";
+// console.log(`"${name5}" is belongs from 'DC' and"${name6}" is belong from 'marvel'`)
+
+//const------->>>>>>>
+// const number=20;
+// console.log(number)
+
+//operators--------->>>>>>>>>>>>>>>
+
+//airthmatic operators
+// let number1=20;
+// let number2=30;
+// console.log( number1+number2)
+// console.log( number1*number2)
+// console.log( number1/number2)
+// console.log( number1-number2)
+// console.log( number1%number2)
+// console.log(++number1,number1++)
+
+// const number = 20
+// console.log(number==20);
+
+//typeof opearators------>>>>>
+let number="20.3"
+console.log(typeof number);//to check the type of the variable
