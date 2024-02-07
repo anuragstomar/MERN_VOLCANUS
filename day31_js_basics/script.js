4562af919a0616d4d1014c48e27b9de6ac463b3d
anuragstomar <anuragstomar121@gmail.comssh-keygen -t ed25519 -C anuragstomar121@gmail.com>
2024-02-07 12:55:51 +0530

    ternary operators, promt, functionintro,switch

---
+++ day31_js_basics/script.js
@@ -0,0 +1,82 @@
+// const age = 18
+// const input =prompt("enter your age=")
+// console.log("user enter age",input)
+// if(input>18){
+//     console.log ("you can  drive")
+//     const city= prompt("enter your city")
+//     if(city!="indore")
+//     {
+//         console.log("you are not eligible")
+//     }
+//     else{
+//     console.log("you are eligible")
+//     }
+// }
+// else{
+//     console.log("you cant drive")
+// }
+
+
+
+
+// const $dollar =500;
+// if($dollar>=500)
+// {
+// console.log("you can purchase an iphone..")
+// }
+// else{
+//     console.log("you have to sale your kidney")
+// }
+
+
+//ternary operators------->>>>>>>>>>
+
+// (10>5) ? (console.log("true")) : (console.log("false"))
+// const $dollar=500;
+// ($dollar>=500)?(console.log("you can purchase an iphone")):(console.log("you have to sell your kidney"))
+
+
+//switch case------>>>>>>>>
+// const day = parseInt(prompt("enter the day"));
+// switch (day)
+// {
+//     case 1:
+//         console.log("monday")
+//         break;
+//         case 2:
+//                 console.log("tuesday")
+//             break;
+//             case 3:
+//                 console.log("wednesday")
+//                 break;
+//                 case 4:
+//                     console.log("thursday")
+//                     break;
+//                     case 5:
+//                         console.log("friday")
+//                         break;
+//                         case 6:
+//                             console.log("saturday")
+//                             break;
+//                             case 7:
+//                                 console.log("sunday")
+//                                 break;   
+//                                 default:
+//                                     console.log("invalid day")
+        
+
+// }
+//normal function----->>>>>
+// function print(){
+//     console.log("this is a function body")
+// }
+// print();
+//function with the return  value------>>>>>>>
+
+// function pi()
+// {
+//     return "superman";
+// }
+
+// const result =pi()
+// console.log(result)
