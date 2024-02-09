18a2f8d07fd60ae8c8a545eeaba6135a1519f8c2
anuragstomar <anuragstomar121@gmail.comssh-keygen -t ed25519 -C anuragstomar121@gmail.com>
2024-02-09 12:07:05 +0530

    loops through array

---
+++ day33_js_4_loop/script.js
@@ -0,0 +1,55 @@
+//array with loop
+//for loop------>>>>>>>>
+// const arr= [10,20,"superman","spiderman",20989.34,400]
+// for(let i=0;i<arr.length;i++)
+// {
+//     console.log(arr[i]);
+// }
+//while loop------>>>>>>>
+
+// let i=0;
+// while(i<arr.length){
+//     console.log(arr[i])
+//     i++;
+// }
+// do{
+//     console.log(arr[i])
+//     i++
+// }
+// while(i<arr.length)
+
+
+// foreach ,map,fillter(the most used loops in JS)
+// const arr= [10,20,"superman","spiderman",20989.34,400,"neetu"]
+
+//for in loop------>>>>>>>
+
+// for(let i in arr)
+// {
+//     console.log("index of element="+i+"actual element ="+arr[i]);
+// }
+
+// for of loop
+// const arr= [10,20,"superman","spiderman",20989.34,400,"neetu"]
+// for(let x of arr)
+// {
+// console.log(x)
+// }
+
+// const person={
+//     name:"ashish",
+//     age:55,
+//     gender:"male",
+//     surname:"yadav",
+//     bestfriend:"gourav",
+//     eyes:2,
+//     nose:1,
+//     legs:2
+
+// }
+// for (let x in person)
+// {
+//     console.log("keys is "+x+" and the value is "+person[x]+"")
+//for each loop------->>>>>>>> 
+// const arr= [10,20,"superman","spiderman",20989.34,400,"neetu"]
+// arr.forEach((value,index,array)=>console.log(value,index,array));
