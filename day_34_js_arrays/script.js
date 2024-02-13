ab5c6d37c1a7038943fc854014e1d792d28c2d64
anuragstomar <anuragstomar121@gmail.comssh-keygen -t ed25519 -C anuragstomar121@gmail.com>
2024-02-13 13:00:28 +0530

    arrays,shift,push,unshift,pop

---
+++ day_34_js_arrays/script.js
@@ -0,0 +1,23 @@
+// let heros = ["sopermna", "spiderman","batman","hulk",120,220,"saktiman" ]
+// let arr= []
+// arr[0]='ram';
+// arr[1]='shaym'
+// arr[2]=20.4
+// console.log(heros)
+//     console.log(arr)
+//     let arrobj=[
+//         {name:"ram",gav:"ayodhya",age:5000},
+//         {name:"ashish",gav:"indore",age:22},
+//         {name:"sahil",gav:"afganistan",age:120},
+//         {name:"supermna",gav:"DC",age:6000},
+//     ];
+//     console.log(arrobj)
+//     console.log(arrobj[1].age)
+
+let arr =[10,20,30,40]
+console.log("original array..",arr)
+arr.push("apple","superman")
+// console.log("pop element=",arr.pop())
+arr.unshift("don3","dr. strange");
+// arr.shift//use to remove the first element
+console.log("new array..",arr)
